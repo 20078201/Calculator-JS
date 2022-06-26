df06209d6b29b162147d916c552994bdc11cf7c2
Dat Ngo <20078201@tafe.wa.edu.au>
2022-06-26 14:20:31 +0800

    Operation() function completed - This will determine what math operation the user wants

--- mathOperation.js
+++ mathOperation.js
@@ -1,3 +1,4 @@
+// Math Operation
 function add(num1, num2) {
     return num1 + num2
 }
@@ -13,3 +14,16 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 }
+
+// Operate function will determine what math operation the user wants
+function operate(num1, num2, operator) {
+    let operation = {
+        '+' : add(num1, num2),
+        '-' : subtract(num1, num2),
+        '*' : multiply(num1, num2),
+        '/' : divide(num1, num2)
+    }
+    return operation[operator] ?? "Operation not found"
+}
+
+console.log(operate(3, 3, '/'))
