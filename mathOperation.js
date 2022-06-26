bed8d2914ef5704d0264b2dc2cd47a4e376fbf45
Dat Ngo <20078201@tafe.wa.edu.au>
2022-06-26 22:35:47 +0800

    Added functionality to calculator

--- mathOperation.js
+++ mathOperation.js
@@ -1,4 +1,9 @@
-// Math Operation
+let displayValue = ''
+let mathOperation = null
+let num1 = null
+let num2 = null  
+
+// Math Operations
 function add(num1, num2) {
     return num1 + num2
 }
@@ -14,20 +19,25 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 }
-
-const calNumber = document.querySelector("#calculator")
-
 // Operate function will determine what math operation the user wants
 function operate(num1, num2, operator) {
     let operation = {
         '+' : add(num1, num2),
         '-' : subtract(num1, num2),
-        '*' : multiply(num1, num2),
+        'x' : multiply(num1, num2),
         '/' : divide(num1, num2)
     }
     return operation[operator] ?? "Operation not found"
 }
+// Update the display Values
+function update() {
+    const container = document.querySelector('div.container')
+    const calculatorContainer = container.querySelector('.calculator')
+    const display = calculatorContainer.querySelector('.display')
+    display.textContent = displayValue
+}
 
+//  Generate Calculator UI 
 function calculatorGenerator() {
     // Array for numbers
     const calNames = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 
@@ -39,7 +49,7 @@ function calculatorGenerator() {
     let operation = {
         'add' : '+',
         'subtract' : '-',
-        'multiply' : 'X',
+        'multiply' : 'x',
         'divide' : '/',
         'equal' : '=',
         'clear' : 'C'
@@ -51,25 +61,57 @@ function calculatorGenerator() {
     const calNumbers = calNumber.querySelectorAll('.cal-number')
     const operations = calNumber.querySelectorAll('.operation')
 
-    for (let i = 0; i < calNames.length; i++) {
-        calNumbers[i].setAttribute('style', `grid-area: ${calNames[i]}`)
-        const button = document.createElement('button')
-        button.classList.add('btn')
-        button.textContent = `${i}`
-        calNumbers[i].appendChild(button)
-    }
-
+    // Create the operation buttons
     for (let index = 0; index < calOperations.length; index++) {
         operations[index].setAttribute('style', `grid-area: ${calOperations[index]}`)
         if (calOperations[index] === 'display') {
             continue
         }
-        
         const button = document.createElement('button')
         button.classList.add('btn')
         button.textContent = operation[calOperations[index]] ?? 'undefine'
+        button.addEventListener('click', () => {
+            if (button.textContent === '+' || button.textContent === '-' ||
+            button.textContent === 'x' || button.textContent === '/') {
+                mathOperation = button.textContent
+                num1 = displayValue
+                displayValue = ''
+                update()    
+            } else if (button.textContent === '=') {
+                num2 = displayValue
+                displayValue = operate(parseInt(num1), parseInt(num2), mathOperation)
+                update()
+            } else if (button.textContent === 'C') {
+                displayValue = ''
+                num1 = null
+                num2 = null
+                mathOperation = null
+                update()
+            }
+            
+
+        })
         operations[index].appendChild(button)
     }
-}
 
+    // Create number buttons
+    for (let i = 0; i < calNames.length; i++) {
+        calNumbers[i].setAttribute('style', `grid-area: ${calNames[i]}`)
+        const button = document.createElement('button')
+        button.classList.add('btn')
+        button.textContent = `${i}`
+        button.addEventListener('click', () => {
+            if  (button.textContent !== '0'){
+                displayValue = displayValue + button.textContent
+            }
+            update()
+        })
+
+        calNumbers[i].appendChild(button)
+    }
+}
+// Invoke function
 calculatorGenerator()
+update()
+
+// 
